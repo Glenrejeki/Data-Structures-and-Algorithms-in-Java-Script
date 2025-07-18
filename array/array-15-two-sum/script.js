d94b2562ada67bfabe8003b3bcaa2edddaad3b6f
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-07-18 15:09:50 +0700

    Two sum Done

---
+++ array/array-15-two-sum/script.js
@@ -0,0 +1,19 @@
+// Note : this is not the better solution
+
+function twoSum(nums, target){
+  // Loop through each number in the list 
+  for(let i = 0 ; i < nums.length ; i++){
+    // For each number,check the rest of the list 
+    for (let j = i + 1 ; j < nums.length; j++){
+      // If the current number and the one we're checking add up to the target, return their indexes
+      if(nums[i] + nums[j] === target){
+        return [i, j]
+      }
+    }
+  }
+  return []
+}
+
+
+const result =  twoSum([2,7,11,16],9)
+console.log(`Hasil penjumlahan dari indeks: `,result)
